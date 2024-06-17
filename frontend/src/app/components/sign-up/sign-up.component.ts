2b09d6e1bd31221119f2051962c7a59e57987dd6
Maximilian Jansen <maxjansen80@gmail.com>
2024-06-17 14:10:25 +0200

    .

--- frontend/src/app/components/sign-up/sign-up.component.ts
+++ frontend/src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from "@angular/forms";
+import {NgClass, NgIf} from "@angular/common";
+import { AuthenticationService } from '@app/utils/services/authentication.service';
+import { StorageService } from '@app/utils/services/storage.service';
 
 @Component({
   selector: 'component-sign-up',
@@ -7,6 +17,31 @@ import { Component } from '@angular/core';
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  private formBuilder: FormBuilder = inject(FormBuilder);
+  private authService = inject(AuthenticationService);
+  private storageService = inject(StorageService)
 
+  form: FormGroup = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+  submitted: boolean = false;
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        firstName: ['', [Validators.required]],
+        lastName: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(40)]],
+      }
+    );
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
 }
