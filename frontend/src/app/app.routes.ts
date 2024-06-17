3a193872846cb8b7ef5df6dff3f8d578551c44c1
femabeh <femabeh@osrc.community>
2024-06-17 10:34:06 +0200

    user-settings init

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {LogoutComponent} from "@app/components/logout/logout.component";
 import {HomeComponent} from "@app/components/home/home.component";
 import {TestComponent} from "@app/components/test/test.component";
 import {PanelComponent} from "@app/components/panel/panel.component";
+import {UserSettingsComponent} from "@app/components/user-settings/user-settings.component";
 
 export const routes: Routes = [
   {
@@ -20,6 +21,10 @@ export const routes: Routes = [
   {
     path: "panels/:identifier",
     component: PanelComponent
+  },
+  {
+    path: "user-settings",
+    component: UserSettingsComponent
   },
   {
     path: "**",
