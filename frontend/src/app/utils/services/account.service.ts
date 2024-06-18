b3583b515b5df372ed7d367d73f914d427273b5a
femabeh <femabeh@osrc.community>
2024-06-18 09:39:27 +0200

    adding account service for creating, updating and deleting accounts.

---
+++ frontend/src/app/utils/services/account.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  constructor() { }
+
+  createAccount() {
+
+  }
+
+  updateAccount() {
+
+  }
+
+  deleteAccount() {
+
+  }
+}
